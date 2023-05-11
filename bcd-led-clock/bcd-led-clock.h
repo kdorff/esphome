5c3f19335f167dfbef98937823f1e5fa3345d687
Kevin Dorff <kdorff@gmail.com>
2023-05-11 17:36:34 -0500

    Refactored for readability and DRY. Fix filenames.

--- bcd-led-clock/bcd-led-clock.h
+++ bcd-led-clock/bcd-led-clock.h
@@ -17,6 +17,8 @@
 #define SECONDS_COL_1_START 16
 #define SECONDS_COL_1_COUNT 4
 
+#define BIT_TO_INT(x) (x == 0) ? ((int) 0) : ((int) 1)
+
 /**
  * Convert an integer to BCD, in four bit groups.
  *    <10         will return one four bit group.
@@ -26,7 +28,7 @@
  *   https://stackoverflow.com/questions/48291111/converting-from-decimal-to-bcd
  */
 std::vector<std::bitset<4>> dec_to_bin(int n) {
-    std::vector<std::bitset<4>> repr;;
+    std::vector<std::bitset<4>> repr;
     while(n > 0){
         repr.push_back(std::bitset<4>(n % 10));
         n /= 10;
@@ -36,12 +38,30 @@ std::vector<std::bitset<4>> dec_to_bin(int n) {
 }
 
 /**
- * Set the "on" LEDS for one column of the clock display.
+ * Set the "on" LEDs for one column of the clock display.
  */
-void turnOnLEDs(esphome::light::AddressableLight &lights, std::bitset<4> &bits, int startLED, int numLEDs, Color &onColor) {
+void setBCDLEDs(esphome::light::AddressableLight &lights, std::bitset<4> &bits, int startLED, int numLEDs, Color &color) {
     for (int i = 0; i < numLEDs; i++) {
         if (bits[i] != 0) {
-            lights[startLED + i] = onColor;
+            lights[startLED + i] = color;
         }
+    }
+}
+
+/**
+ * Set the LEDs for one group (hour, minute, or second).
+ */
+void setLEDGroup(
+        esphome::light::AddressableLight &lights, 
+        std::vector<std::bitset<4>> &bcd, 
+        int col0StartLED, int col0NumLEDs, 
+        int col1StartLED, int col1NumLEDs, 
+        Color &color) {
+    if (bcd.size() == 1) {
+        setBCDLEDs(lights, bcd[0], col1StartLED, col1NumLEDs, color);
+    }
+    else if (bcd.size() == 2) {
+        setBCDLEDs(lights, bcd[0], col0StartLED, col0NumLEDs, color);
+        setBCDLEDs(lights, bcd[1], col1StartLED, col1NumLEDs, color);
     }
 }
